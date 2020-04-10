243db5e372f239c38909018bfb2fb845898ae592
Jackie <kamprat2@gmail.com>
2020-04-10 10:56:53 -0400

    markers!!!

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2FtcHJhdDIiLCJhIjoiY2s4amM3YnBmMGZjODNmbXZxMHRnZDI0MSJ9.Zm94QCrl8KBL1xzGcyoQzg";
---
+++ static/js/logic.js
@@ -0,0 +1,31 @@
+//Level 1: Basic Visualizations
+
+//create myMap & add in the map layer
+var myMap = L.map("map", {
+    center: [39.8283, -98.579],
+    zoom: 5
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+//choose dataset - All Earthquakes in the past 1 Day
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+//create a circles variable
+var circles;
+
+//read in the earthquake data
+d3.json(link, function(data) {
+  console.log(data);
+
+  L.geoJson(data).addTo(myMap);
+  });
+
+    
+
+
